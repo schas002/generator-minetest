9d4cc06c2bdf28b489e85eb617e73bb8d8375a26
Andrew Zyabin <schas002@users.noreply.github.com>
2017-02-11 13:35:10 +0400

    Confuse Lua expressions with JavaScript >_>

--- generators/app/templates/init.lua
+++ generators/app/templates/init.lua
@@ -1,6 +1,6 @@
 MODNAME = "<%= modname %>"
 VERSION = "<%= "1/" + new Date().toISOString().substring(2, 10).replace(/-/g, "") %>"
 
-print(MODNAME + " version " + VERSION + " up")
+print(MODNAME .. " version " .. VERSION .. " up")
 print("Powered by the bedazzling generator-minetest") -- You may remove this
                                                       -- line, if you want
